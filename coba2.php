8ab3cf0607783b236cbbbaa2754afa3d8f6a6a99
Hasim-febri <92961707+Hasim-febri@users.noreply.github.com>
2021-10-22 08:24:56 +0700

    Add files via upload

---
+++ coba2.php
@@ -0,0 +1,108 @@
+<div class="col-sm-9 col-sm-offset-3 col-lg-10 col-lg-offset-2 main">
+    <div class="row">
+      <div class="col-lg-12">
+        <!-- /.panel-->
+        <div class="panel panel-default">
+          <div class="panel-body">
+            <div class="col-md-6 col-md-offset-3">
+              <style type="text/css">
+.tftable {font-size:12px;color:#333333;margin-left:10%;width:80%;margin-right:10%;border-width: 1px;border-color: #729ea5;border-collapse: collapse;float:center;}
+.tftable th {font-size:12px;border-width: 1px;padding: 8px;border-style: solid;border-color: #729ea5;text-align:center;}
+.tftable tr {background-color:#fff;}
+.tftable td {font-size:11px;border-width: 1px;padding: 8px;border-style: solid;border-color: #729ea5;}
+.tftable tr:hover {background-color:#ffffff;}
+</style>
+               
+                
+               <table border="1" style="margin-right:10%;margin-left:10%;">
+               	<tr><td>
+               <br>
+                <div style="clear: both;"></div>
+                <!-- <center>BERITA ACARA REKONSILIASI <br> JUMLAH PEKERJA DAN BESARAN IURAN JAMINAN KESEHATAN NASIONAL ANTARA BPJS KESEHATAN KANTOR CABANG MAGELANG TAHUN 2020 DENGAN SLB NEGERI TEMANGGUNG</h4></center><br><br> -->
+                <div style="text-align: center;"><font size="2"><b>BERITA ACARA REKONSILIASI<br> Iuran Jaminan Kesehatan Peserta Pekerja Penerima Upah (PPU) Pemerintah Daerah <br>Antara KPPN……,Pemerintah Daerah Prov/Kab/Kota….., dan BPJS Kesehatan Kantor Cabang…. <br>Triwulan ….. Tahun 2020<br>Nomor: …../……./…..<br>Nomor: …../……./…..  </b></font><!-- </center> --></div><br>
+              <p style="margin-right:10%;margin-left:10%;">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+              Pada hari ini...tanggal...bulan...tahun...(.../.../...) bertempat di... telah dilakukan Rekonsiliasi Iuran Wajib Pemerintah Daerah/Kab/Kota...untuk periode...sd...tahun..., antar <font color="RED">Satker</font> dan BPJS Kesehatan Cabang... dengan hasil sebagai berikut :</p> 
+              <table class="tftable" border="1">
+              <tr><th rowspan="2">No</th><th rowspan="2">Bulan</th><th >Iuran 1% Seharusnya</th><th>Iuran 4% Seharusnya</th></tr>
+              <tr><td>PPNPN</td><td>PPNPN</td></tr>
+                  <tr>
+                    <td>1</td>
+                    <td>2</td>
+                    <td>3</td>
+                    <td>4</td>
+                  </tr>
+                  <tr>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                  </tr>
+                  <tr><th colspan="2">Tahun </th> <td></td><td></td></tr></table><br>
+                  <table class="tftable" border="1">
+              <tr><th rowspan="2">No</th><th rowspan="2">Bulan</th><th >Iuran 1% Seharusnya</th><th>Iuran 4% Seharusnya</th></tr>
+              <tr><td>PPNPN</td><td>PPNPN</td></tr>
+                  <tr>
+                    <td>1</td>
+                    <td>2</td>
+                    <td>3</td>
+                    <td>4</td>
+                  </tr>
+                  <tr>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                  </tr>
+                  <tr><th colspan="2">Tahun </th> <td></td><td></td></tr></table><br>
+                  <table class="tftable" border="1">
+              <tr><th rowspan="2">No</th><th rowspan="2">Bulan</th><th >Iuran 1% Seharusnya</th><th>Iuran 4% Seharusnya</th></tr>
+              <tr><td>PPNPN</td><td>PPNPN</td></tr>
+                  <tr>
+                    <td>1</td>
+                    <td>2</td>
+                    <td>3</td>
+                    <td>4</td>
+                  </tr>
+                  <tr>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                  </tr>
+                  <tr><th colspan="2">Tahun </th> <td></td><td></td></tr></table>
+              
+              <br>
+             <p style="margin-right:5%;margin-left:10%;">
+              *) Berdasarkan hasil perhitungan tersebut diatas, kekurangan setoran iuran jaminan kesehatan Triwulan I Tahun 2020 akan dibayarkan oleh Pemerintah Prov/Kab/Kota…….  pada bulan..…… <br>
+          	 *) Berdasarkan hasil perhitungan tersebut diatas, kelebihan setoran iuran jaminan kesehatan Triwulan I Tahun 2020 akan dikompensasikan untuk iuran bulan……
+          	</p>
+          	<p style="margin-right:5%;margin-left:10%;">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+              Rincian perhitungan iuran jaminan kesehatan tersaji dalam lampiran Berita Acara yang merupakan bagian yang tidak terpisahkan dari Berita Acara Hasil Rekonsiliasi ini. <br><br>
+              &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Apabila dikemudian hari terhadap hasil rekonsiliasi masih terdapat selisih perhitungan sebagai akibat perbedaan data, berita acara yang telah ditetapkan akan dilakukan perbaikan sebagaimana mestinya.<br><br>
+          	  Demikian Berita Acara Rekonsiliasi ini dibuat dengan sebenar-benarnya dan untuk dipergunakan sebagaimana mestinya.</p>
+          	  <br>
+              <div style="float:right;margin-right:10%;text-align:center;"><font size="2">Temanggung 07/01/2020</font></div><br><br>
+
+              <div style="float:left;margin-left:10%;text-align:center;"><font size="2">PIHAK PERTAMA <br>
+             BPJS KESEHATAN KC MAGELANG
+              <br><br><br><br><u>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</u><br></font>
+              </div>
+              <div style="float:right;margin-right:10%;text-align:center;"><font size="2">PIHAK KEDUA<br>
+              <b>Satker</b>
+              <br><br><br><br><u>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</u><br></font>
+              </div>
+              </div></td></tr></table>
+              <p style="margin-right:5%;margin-left:10%;">
+              *) Pilih salah satu sesuai kondisi hasil perhitungan kelebihan/kekurangan setor iuran  
+          	</p>
+          	<p style="margin-left:10%;margin-right:5%;background-color:RED "><font color="white">Catatan: Mohon untuk tidak menginsert kolom ataupun mengubah rumus pada kolom yang berwarna abu-abu. Silahkan menginsert row untuk menambahkan bulan rekon</font>
+          		
+          	</p>
+
+
+            </div>
+          </div>
+        </div><!-- /.panel-->
+      </div>
+  </div>
+</div>
